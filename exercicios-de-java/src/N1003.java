fdbfa6a70c0cb6e5a656e7e4ce0e9b67ca37b83b
William Almeida <79884134+Crkw1@users.noreply.github.com>
2022-01-27 10:31:53 -0300

    Meus exercicios de programação Java.

---
+++ exercicios-de-java/src/N1003.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+public class N1003 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int soma = a + b;
+        System.out.println("SOMA = " + soma);
+
+    }
+}
