f826d132d1821c5473ee1ef1379fa18a2a676043
William Almeida <79884134+Crkw1@users.noreply.github.com>
2022-01-29 00:41:27 -0300

    Projeto DIO GFT utilizando Spring, Strategy, Facade, Singleton.

---
+++ lab-padroes.projeto-spring/src/main/java/desafiofinalgtf/Application.java
@@ -0,0 +1,19 @@
+package desafiofinalgtf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Crkw1
+ */
+
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
