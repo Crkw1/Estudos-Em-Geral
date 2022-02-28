d343cc336d6e832e19e7a9747c64f96976d51709
William Almeida <79884134+Crkw1@users.noreply.github.com>
2022-02-27 23:16:19 -0300

    Delete gerenciaador-de-reserva directory

--- gerenciaador-de-reserva/src/main/java/com/gerenciador/reserva/gerenciaador_de_reserva/repos/ReservaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gerenciador.reserva.gerenciaador_de_reserva.repos;
-
-import com.gerenciador.reserva.gerenciaador_de_reserva.domain.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-}
--- gerenciaador-de-reserva/src/main/java/com/gerenciador/reserva/gerenciaador_de_reserva/repos/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gerenciador.reserva.gerenciaador_de_reserva.repos;
-
-import com.gerenciador.reserva.gerenciaador_de_reserva.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
